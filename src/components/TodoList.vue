d2b868df90ba71fbcd1b030eeb63edbd9dcd1f44
pidokige02 <pidokige0204@gmail.com>
2022-10-29 20:23:48 +0900

    Reactive Properties in Pinia

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,24 @@
 <script setup>
+import { useTodoListStore } from '@/stores/todoList'
+import { storeToRefs } from 'pinia'
+const store = useTodoListStore();
+// storeToRefs lets todoList keep reactivity:
+const { todoList } = storeToRefs(store)
+// destructuring action method doesn't require using storeToRefs:
+const { toggleCompleted } = store
 </script>
 
 <template>
-  <div></div>
+  <div v-for="todo in todoList" :key="todo.id" class="list">
+    <div class="item">
+      <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
+      <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.completed {
+  text-decoration: line-through;
+}
 </style>
--- src/stores/todoList.js
+++ src/stores/todoList.js
@@ -13,6 +13,13 @@ export const useTodoListStore = defineStore('todoList', {
       this.todoList = this.todoList.filter((object) => {
         return object.id !== itemId
       })
+    },
+    toggleCompleted(idToFind) {
+      // todoList 안에 있는 원본이 todo 가 가지고 있다. copy 가 아니다.
+      const todo = this.todoList.find((obj) => obj.id === idToFind);
+      if (todo) {
+        todo.completed = !todo.completed
+      }
     }
   }
 })
